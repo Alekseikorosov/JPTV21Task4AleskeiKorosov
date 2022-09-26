a34f07f829a95445363fb4eafd8d9ba3bfdb83dd
pupil <pupil@ivkhk.ee>
2022-09-26 15:22:59 +0300

    first commit

---
+++ JPTV21Task4.java
@@ -0,0 +1,19 @@
+package jptv21task4;
+
+import java.util.Random;
+
+public class JPTV21Task4 {
+
+    public static void main(String[] args) {
+        int[] i = new int[100];
+        Random rnd = new Random();
+        for (int i = 0; i <= 100; i++) {
+            if (i % 2 != 0)  {
+                System.out.println(i);
+                    
+                    
+            }   
+        
+        }
+    }
+}
